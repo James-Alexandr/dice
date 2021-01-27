4b5e9b4fc1b5fb11f84de77e829175a7c5a2261f
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-01-27 13:47:25 -0600

    added funtion

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-alert('Hello world');
+
+
+var ramdonNumber1 = Math.floor(Math.random() * 6)+1;
+
+var ramdonImage = "dice" + ramdonNumber1 +".png";
+
+var ramdonImageSource = "images/" + ramdonImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", ramdonImageSource );
+
+
+
+var ramdonNumber2 = Math.floor(Math.random() * 6)+1;
+
+var ramdonImage2 = "dice" + ramdonNumber2 +".png";
+
+var ramdonImageSource2 = "images/" + ramdonImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", ramdonImageSource2 );
+
+if (ramdonNumber1 > ramdonNumber2 ){
+document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+
+if (ramdonNumber1 < ramdonNumber2 ){
+  document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+
+if (ramdonNumber1 == ramdonNumber2 ){
+  document.querySelector("h1").innerHTML = "It is a Draw!";
+
+}
