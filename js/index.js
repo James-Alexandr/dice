ddfb4ac1e2fab80d5e9f083e329f2f276b772df9
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-01-27 14:23:45 -0600

    added button

--- js/index.js
+++ js/index.js
@@ -1,36 +1,38 @@
 
+function rollDice(){
+  var ramdonNumber1 = Math.floor(Math.random() * 6)+1;
 
-var ramdonNumber1 = Math.floor(Math.random() * 6)+1;
+  var ramdonImage = "dice" + ramdonNumber1 +".png";
 
-var ramdonImage = "dice" + ramdonNumber1 +".png";
+  var ramdonImageSource = "images/" + ramdonImage;
 
-var ramdonImageSource = "images/" + ramdonImage;
+  var image1 = document.querySelectorAll("img")[0];
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", ramdonImageSource );
+  image1.setAttribute("src", ramdonImageSource );
 
 
 
-var ramdonNumber2 = Math.floor(Math.random() * 6)+1;
+  var ramdonNumber2 = Math.floor(Math.random() * 6)+1;
 
-var ramdonImage2 = "dice" + ramdonNumber2 +".png";
+  var ramdonImage2 = "dice" + ramdonNumber2 +".png";
 
-var ramdonImageSource2 = "images/" + ramdonImage2;
+  var ramdonImageSource2 = "images/" + ramdonImage2;
 
-var image2 = document.querySelectorAll("img")[1];
+  var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", ramdonImageSource2 );
+  image2.setAttribute("src", ramdonImageSource2 );
 
-if (ramdonNumber1 > ramdonNumber2 ){
-document.querySelector("h1").innerHTML = "Player 1 wins!";
-}
+  if (ramdonNumber1 > ramdonNumber2 ){
+  document.querySelector("h1").innerHTML = "Player 1 wins!";
+  }
 
-if (ramdonNumber1 < ramdonNumber2 ){
-  document.querySelector("h1").innerHTML = "Player 2 wins!";
-}
+  if (ramdonNumber1 < ramdonNumber2 ){
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+  }
 
-if (ramdonNumber1 == ramdonNumber2 ){
-  document.querySelector("h1").innerHTML = "It is a Draw!";
+  if (ramdonNumber1 == ramdonNumber2 ){
+    document.querySelector("h1").innerHTML = "It is a Draw!";
+
+  }
 
 }
